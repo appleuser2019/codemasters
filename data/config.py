57dfe84db0c84985bbd5b72080ef9bab878e8892
DEDinPALTO <57043824+appleuser2019@users.noreply.github.com>
2023-09-14 12:48:36 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -3,5 +3,5 @@ from environs import Env
 env = Env()
 env.read_env()
 
-BOT_TOKEN = "6114540894:AAEz-XaYiIZkon74cPasvP_Py1_7MwKffw8"
-IP = "localhost"
+BOT_TOKEN = env.str("BOT_TOKEN")
+IP = env.str("ip")
